c59ab45625cac40720d569e07a710a7ec2d98be6
leedongyeop <ldy_1204@naver.com>
2022-03-16 18:54:27 +0900

    이것이 자바다 18장 예제코드 일부 추가

---
+++ src/newCode/thisisjavaExercise/chapter18/ConsoleExample.java
@@ -0,0 +1,20 @@
+package newCode.thisisjavaExercise.chapter18;
+
+import java.io.Console;
+
+public class ConsoleExample {
+    public static void main(String[] args) {
+        Console console = System.console();
+
+        System.out.print("ID: ");
+        String id = console.readLine();
+
+        System.out.print("PW: ");
+        char[] charPW = console.readPassword();
+        String pw = new String(charPW);
+
+        System.out.println("----------------------");
+        System.out.println(id);
+        System.out.println(pw);
+    }
+}
