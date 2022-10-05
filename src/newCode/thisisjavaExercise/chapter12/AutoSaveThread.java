e8a50f66d065dbc79077984b01085bd569126abb
leedongyeop <ldy_1204@naver.com>
2022-10-05 23:53:26 +0900

    데몬 스레드

---
+++ src/newCode/thisisjavaExercise/chapter12/AutoSaveThread.java
@@ -0,0 +1,19 @@
+package src.newCode.thisisjavaExercise.chapter12;
+
+public class AutoSaveThread extends Thread {
+    public void save() {
+        System.out.println("작업 내용을 저장함.");
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                break;
+            }
+            save();
+        }
+    }
+}
